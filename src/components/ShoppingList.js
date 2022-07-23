b703bb34a1f2c3cf0f29512d94cf005a6af81806
George <gnjuki19@gmail.com>
2022-07-24 00:16:48 +0300

    Practiced state and events in react

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleChange(event){
+    if(event.target.value === "All"){
+      setSelectedCategory(items)
+    }else if(event.target.value === "Produce"){
+      setSelectedCategory(items[1])
+    }else if(event.target.value === "Dairy"){
+      setSelectedCategory(items[2])
+    }else{
+      setSelectedCategory(items[3])
+    }
+
+  }
+
+  // I didnt finish up the above function. Got tired and the filter method 
+  // just couldn't click in my mind. 
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
